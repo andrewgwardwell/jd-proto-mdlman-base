d0c0a1603ed63f64eec69ebe3767afdaf88fcc65
Andrew Wardwell <AWardwell@andrews-mbp.myhome.westell.com>
2015-10-14 00:28:44 -0400

    2 demos added and layouts etc

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,19 @@
 //= require_tree .
+
+
+(function($){
+	slick_inst = {
+		init: function(){
+			$('.component--slider').slick({
+				centerMode: true,
+				dots: true,
+			});
+		}
+	};	
+
+
+	$(document).ready(function(){
+		slick_inst.init();
+
+	});
+})(jQuery);
